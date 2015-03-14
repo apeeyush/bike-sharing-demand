705fd398b2580ce3fe8c101ea391f44f380958f3
apeeyush <peeyushagarwal1994@gmail.com>
2015-03-14 15:20:20 +0530

    Add time_since_epoch feature to rf

--- code/random_forest.py
+++ code/random_forest.py
@@ -14,6 +14,7 @@ def rmsle(predicted, actual):
     return sqrt(error/len(actual))
 
 def transform_data(df):
+    epoch = datetime.utcfromtimestamp(0)
     datetime_values = df['datetime'].values
     time_values = []
     date_values = []
@@ -21,6 +22,7 @@ def transform_data(df):
     year_values =[]
     weekday_values = []
     isSunday_values = []
+    time_since_epoch_values = []
     for datetime_value in datetime_values:
         datetime_object = datetime.strptime(datetime_value, '%Y-%m-%d %H:%M:%S')
         time_values.append(datetime_object.hour)
@@ -29,12 +31,14 @@ def transform_data(df):
         year_values.append(datetime_object.year)
         weekday_values.append(datetime_object.weekday())
         isSunday_values.append(1 if datetime_object.weekday() == 6 else 0)
+        time_since_epoch_values.append(int((datetime_object-epoch).total_seconds()/3600))
     df['time'] = time_values
     df['date'] = date_values
     df['month'] = month_values
     df['year'] = year_values
     df['weekday'] = weekday_values
     df['isSunday'] = isSunday_values
+    df['time_since_epoch'] = time_since_epoch_values
     return df
 
 
@@ -52,27 +56,27 @@ test_df = transform_data(test_df)
 # df['time'] = df['time'].astype('category')
 # print df.dtypes
 
-features = ['season','holiday','workingday','weather','temp','atemp','humidity','windspeed','time','weekday','month', 'year', 'isSunday']
+features = ['season','holiday','workingday','weather','temp','atemp','humidity','windspeed','time','weekday','month', 'year', 'isSunday', 'time_since_epoch']
 train_features = features + ['count']
 train_data = df[train_features].values
 test_data = test_df[features].values
 
-# Train rf
-forest = RandomForestRegressor(n_estimators=120)
-forest.fit(train_data[0::,1:-1], train_data[0::,-1])
-output = forest.predict(test_data[0::,1::]).astype(int)
-# Prepare Kaggle Submission
-datetimes = test_df['datetime'].values
-predictions_file = open("../data/random_forest_submission.csv", "wb")
-open_file_object = csv.writer(predictions_file)
-open_file_object.writerow(["datetime","count"])
-open_file_object.writerows(zip(datetimes, output))
-predictions_file.close()
-print 'Done.'
-
-# # Validation
-# X_train, X_test, y_train, y_test = train_test_split(train_data[0::,1:-1], train_data[0::,-1], test_size=0.2, random_state=0)
+# # Train rf
 # forest = RandomForestRegressor(n_estimators=120)
-# forest.fit(X_train, y_train)
-# output = forest.predict(X_test)
-# print rmsle(output, y_test)
+# forest.fit(train_data[0::,1:-1], train_data[0::,-1])
+# output = forest.predict(test_data[0::,1::]).astype(int)
+# # Prepare Kaggle Submission
+# datetimes = test_df['datetime'].values
+# predictions_file = open("../data/random_forest_submission.csv", "wb")
+# open_file_object = csv.writer(predictions_file)
+# open_file_object.writerow(["datetime","count"])
+# open_file_object.writerows(zip(datetimes, output))
+# predictions_file.close()
+# print 'Done.'
+
+# Validation
+X_train, X_test, y_train, y_test = train_test_split(train_data[0::,1:-1], train_data[0::,-1], test_size=0.2, random_state=0)
+forest = RandomForestRegressor(n_estimators=120)
+forest.fit(X_train, y_train)
+output = forest.predict(X_test)
+print rmsle(output, y_test)
