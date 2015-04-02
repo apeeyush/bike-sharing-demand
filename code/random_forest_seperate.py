26ef3c822559f3dde95c56c83039045d601ac91c
apeeyush <peeyushagarwal1994@gmail.com>
2015-04-03 00:43:53 +0530

    Add code for separate casual and registered prediction in rf

--- code/random_forest_seperate.py
+++ code/random_forest_seperate.py
@@ -10,7 +10,7 @@ from datetime import datetime
 def rmsle(predicted, actual):
     error = 0
     for i in range(len(actual)):
-        error += pow(log(actual[i]+1)-log(predicted[i]+1), 2)
+        error += pow(actual[i]-predicted[i], 2)
     return sqrt(error/len(actual))
 
 def transform_data(df):
@@ -39,13 +39,19 @@ def transform_data(df):
     df['weekday'] = weekday_values
     df['isSunday'] = isSunday_values
     df['time_since_epoch'] = time_since_epoch_values
+    # df['log-count'] = df['count']
     return df
+
 
 df = pd.read_csv('../data/train.csv')
 test_df = pd.read_csv('../data/test.csv')
 
 df = transform_data(df)
 test_df = transform_data(test_df)
+
+df['count'] = [log(1+x) for x in df['count']]
+df['casual'] = [log(1+x) for x in df['casual']]
+df['registered'] = [log(1+x) for x in df['registered']]
 
 # # Convert data to categorical
 # df['weather'] = df['weather'].astype('category')
@@ -56,8 +62,9 @@ test_df = transform_data(test_df)
 # print df.dtypes
 
 features = ['season','holiday','workingday','weather','temp','atemp','humidity','windspeed','time','weekday','month', 'year', 'isSunday', 'time_since_epoch']
-train_features = features + ['count']
-train_data = df[train_features].values
+train_features = features
+X_train_data = df[train_features].values
+y_train_data = df[['casual', 'registered']].values
 test_data = test_df[features].values
 
 # # Train rf
@@ -74,9 +81,19 @@ test_data = test_df[features].values
 # print 'Done.'
 
 # Validation
-X_train, X_test, y_train, y_test = train_test_split(train_data[0::,0:-1], train_data[0::,-1], test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X_train_data[0::,0::], y_train_data[0::,0::], test_size=0.2, random_state=0)
+print X_train.shape, y_train.shape
+## Casual
 forest = RandomForestRegressor(n_estimators=120)
-forest.fit(X_train, y_train)
-output = forest.predict(X_test)
-print rmsle(output, y_test)
+forest.fit(X_train, y_train[0::,0])
+output1 = forest.predict(X_test)
+## Resistered
+forest = RandomForestRegressor(n_estimators=120)
+forest.fit(X_train, y_train[0::,1])
+output2 = forest.predict(X_test)
+output1 = [int(exp(x)-1) for x in output1]
+output2 = [int(exp(x)-1) for x in output2]
+output = output1+output2
+output = [log(1+x) for x in output]
+print rmsle(output, df[['count']].values[0])
 print forest.feature_importances_
